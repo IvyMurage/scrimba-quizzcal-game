127a373216160a6fcd51457f26d9a18fbbeb8ac2
Murage-Ivy <ivymurage2000@gmail.com>
2024-07-25 16:44:28 +0200

    feat: Configure tailwind chore: install react router dom feat: link home and quiz page feat: Create and design the home Page

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
+import Quiz from "./components/Quiz"
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+function Home() {
+    return (
+        <div className="space-y-3 flex flex-col items-center justify-center h-screen bg-primary">
+            <h1 className="font-bold text-xl">Quizzcal</h1>
+            <p className="text-sm">Some description if needed</p>
+            <Link to='/quiz'><button className=" text-sm font-semibold rounded-lg px-5 py-2 text-primary bg-secondary">
+                Start Quiz
+            </button>
+            </Link>
+        </div>
+    )
+}
+
+export default Home
